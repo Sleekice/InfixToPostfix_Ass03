ccaba3220d0267eb4f16d387572b45050f5cab12
lu0825et <bigICE01@>
2020-09-23 15:51:39 -0500

    file update as of 9/23/20

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,74 @@
+import java.io.*;
+
 public class Parser {
+    static int lookahead;
+
+    public Parser() throws IOException {
+        lookahead = System.in.read();
+    }
+
+    void expr() throws IOException {
+        Noname();
+
+        while (true) {
+            if (lookahead == '+') {
+                match('+');
+                Noname();
+                System.out.write('+');
+            } else if (lookahead == '-') {
+                match('-');
+                Noname();
+                System.out.write('-');
+            } else {
+                return;
+            }
+        }
+    }
+    void Noname() throws IOException {
+        term();
+
+        while (true) {
+            if (lookahead == '*') {
+                match('*');
+                term();
+                System.out.write('*');}
+            else if (lookahead == '/') {
+                match('/');
+                term();
+                System.out.write('/');
+            } else if (lookahead == '%') {
+                match('%');
+                term();
+                System.out.write('%');
+            } else {
+                return;
+            }
+        }
+    }
+    void term() throws IOException {
+        if (Character.isDigit((char) lookahead)) {
+            System.out.write((char) lookahead);
+            match(lookahead);
+        }
+        else if (Character.isLetter((char) lookahead)) {
+            System.out.write((char) lookahead);
+            match(lookahead);}
+        else if (lookahead == '(') {
+            match('(');
+            expr();
+            match(')');
+        }
+        else {
+            throw new Error("syntax error");
+        }
+    }
+
+    void match(int t) throws IOException {
+        if (lookahead == t) {
+            lookahead = System.in.read();
+        } else {
+            throw new Error("syntax error");
+
+        }
+    }
 }
--- src/Postfix.java
+++ src/Postfix.java
@@ -1,2 +1,13 @@
+import java.io.IOException;
+
 public class Postfix {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.print("Enter Characters: ");
+        Parser parse = new Parser();
+        parse.expr();
+        System.out.println("");
+
+    }
 }
